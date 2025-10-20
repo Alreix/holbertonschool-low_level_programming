0fb89df148bc799773569ca382b9b07447447e52
Alreix <alreixdev@gmail.com>
2025-10-20 15:10:34 +0200

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu\n", sizeof(char);
+	printf("Size of int: %lu\n", sizeof(int);
+	printf("Size of long: %lu\n", sizeof(long);
+	printf("Size of long long int: %lu\n", sizeof(long long);
+	printf("Size of float: %lu\n", sizeof(float);
+	return (0);
+}
