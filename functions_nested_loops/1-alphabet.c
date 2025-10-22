b147ca219fadb9be3a1712bd3e7e78e62e428054
Alreix <alreixdev@gmail.com>
2025-10-22 11:51:41 +0200

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,27 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print the alphabet.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char c;
-	for(c = 'a'; c <= 'z'; c++)
-	_putchar(c);
-}
+	print_alphabet();
+	_putchar('\n');
 
+	return (0);
+}
