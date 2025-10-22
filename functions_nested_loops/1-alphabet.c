4fcc3773ee64e22370badff26e897ec93376f8b5
Alreix <alreixdev@gmail.com>
2025-10-22 12:05:28 +0200

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -12,16 +12,3 @@ void print_alphabet(void)
 	for (c = 'a'; c <= 'z'; c++)
 	_putchar(c);
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-
-	return (0);
-}
