0145f67b941ab834f9bfcbab547297a120d03fcd
Alreix <alreixdev@gmail.com>
2025-10-22 13:56:38 +0200

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - Print 10 times alphabet
+ * print_alphabet_x10 - Print 10 times alphabet
  *
+ * Description: Use while and for to print 10 times alphabet.
  * Return: void
  */
 void print_alphabet_x10(void)
@@ -10,9 +11,9 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char c;
 
-	while(i < 10)
+	while (i < 10)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
