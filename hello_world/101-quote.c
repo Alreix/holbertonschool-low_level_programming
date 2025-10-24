a785349686407ffed0d05219a25ff4af541d8e54
Alreix <alreixdev@gmail.com>
2025-10-24 11:17:05 +0200

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+
+	return (1);
+}
