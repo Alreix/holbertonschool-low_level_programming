5886d23b059069dee42bd7eae06f47c31b358a5c
Alreix <alreixdev@gmail.com>
2025-10-27 14:40:03 +0100

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - to print a square.
+ * @size: the size of the square.
+ *
+ * Description: print a square.
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
+
