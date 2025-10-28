f0f9f2437a3ebd0348769f38ad7eaf91e592fc14
Alreix <alreixdev@gmail.com>
2025-10-28 13:51:36 +0100

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - puts every other character of a string if it is even.
+ * @str: The string.
+ *
+ * Description: puts only even numbers.
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+
+		}
+		
+		len++;
+	}
+	_putchar('\n');
+}
