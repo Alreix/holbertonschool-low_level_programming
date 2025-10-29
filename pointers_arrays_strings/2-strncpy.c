df912b0b62d76d452e05f09f57fb778474bf20ec
Alreix <alreixdev@gmail.com>
2025-10-29 16:31:13 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncpy - to copies a string.
+ * @dest: The destination of a string.
+ * @src: the source string.
+ * @n: the number of character to copies.
+ *
+ * Description: to copy a string like strncpy.
+ * Return: dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *tmp = dest;
+
+	for(i = 0; i < n; i++)
+	{
+		if (*src != '\0')
+		{	
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		else
+		*dest = '\0';
+	}
+
+	return (tmp);
+}
