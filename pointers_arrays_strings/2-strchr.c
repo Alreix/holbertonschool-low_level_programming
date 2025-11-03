e65a10cf11b499b93aa2543f18aaa28f749452f0
Alreix <alreixdev@gmail.com>
2025-11-03 11:14:03 +0100

    add the personnalised function strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - Locate the first occurrence of a character in a string.
+ * @s: String to scan.
+ * @c: Character to find.
+ *
+ * Return: Pointer to first occurrence in s, or 0 if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+
+		s++;
+	}
+	return (0);
+}
