e1df856f0837d37ccd32a47dd7d3b4ff7539df1c
Alreix <alreixdev@gmail.com>
2025-11-03 11:51:09 +0100

    fixed condition of if '\0' in strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,12 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (s);
-
-		else if (*s == '\0')
-			return (s);
-
+		return (s);
 		s++;
 	}
+
+	if (c == '\0')
+		return (s);
+
 	return (0);
 }
