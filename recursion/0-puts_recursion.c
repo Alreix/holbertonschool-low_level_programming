298d61dbe1504eae117aeadf59042ce7f7e545e8
Alreix <alreixdev@gmail.com>
2025-11-04 11:53:12 +0100

    correction of trailing whitespace in the file

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _puts_recursion - Checks if a character is lowercase.
- * @c: The character to check.
+ * _puts_recursion - to prints a string, followed by new line.
+ * @s: the pointer to 1st character of a string.
  *
- * Description: 
- * Return: 1 if c is lowercase, 0 otherwise.
+ * Description: a function for prints a string with recursivity.
+ * Return: void.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0' || *s == 0)
+	if (*s != '\0')
+		_putchar(*s);
+		return;
+
+	else
 	{
 		_putchar('\n');
-		return;
 	}
 
-	_putchar(*s);
-	
 	_puts_recursion(s + 1);
 }
