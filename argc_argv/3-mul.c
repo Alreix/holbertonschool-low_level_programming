6b1ec74147313f95a09e5de6fbbb3a8e347bd738
Alreix <alreixdev@gmail.com>
2025-11-05 13:43:20 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,18 +16,18 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 1;
 
-	if (argc > 1)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			sum *= atoi(argv[i]);
 		}
-	}
-
-	else if (argc == 1)
-	{
-		printf("Error\n");
-		return (1);
 	}
 
 	printf("%d\n", sum);
