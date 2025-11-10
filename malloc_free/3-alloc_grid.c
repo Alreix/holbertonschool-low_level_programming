72b56f701b0c53a5b42022993cb1a030e1c7930e
Alreix <alreixdev@gmail.com>
2025-11-10 19:35:33 +0100

    fixed bug of compilation (the condition of grid[i] == NULL

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,10 +26,12 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		grid[i] = malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 			return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
