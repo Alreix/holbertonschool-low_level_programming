96c9f921c4ed8b05fa5dcfe38029291c986bd90f
Alreix <alreixdev@gmail.com>
2025-11-17 13:41:05 +0100

    add function for operator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the difference of a and b.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - check the remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
+
