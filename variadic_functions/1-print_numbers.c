38db85223bebd6ee504db7dc6c3bdea1a9fc501c
Alreix <alreixdev@gmail.com>
2025-11-19 10:36:52 +0100

    fix condition for separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,10 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
-			printf("%i%s", va_arg(ap, int), separator);
+		if (separator != NULL && i != n - 1)
+			printf("%d%s", va_arg(ap, int), separator);
 		else
-			printf("%i", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
 	}
 	va_end(ap);
 	printf("\n");
