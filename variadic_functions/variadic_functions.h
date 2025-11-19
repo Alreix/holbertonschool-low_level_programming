eb81f4f7b04d527e8b8af616d120ee1a8cd782b6
Alreix <alreixdev@gmail.com>
2025-11-19 10:11:21 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdarg.h>
+
+/**
+ * struct printer - Struct for type and function
+ * @sym: Format symbol.
+ * @print: Function to print this type.
+ */
+typedef struct printall
+{
+	char sym;
+	void (*print)(va_list ap);
+
+}printer_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_str(va_list ap);
 
 #endif
